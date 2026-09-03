4b6b10494ea33b16d55d8f8daaeb5d1984b6f480
022247 <022247@hafs.hs.kr>
2026-09-03 16:51:55 +0900

    Update LogicNode to include subscript formatting
    Modified LogicNode class to format output with subscript in expression text.

--- SimulatorEngine.js
+++ SimulatorEngine.js
@@ -1,16 +1,15 @@
+// (기존 구조와 동일하나 출력 시 아래첨자를 수식 텍스트에도 반영하기 위해 소폭 수정)
 class LogicNode {
     constructor(id, type) {
         this.id = id;
-        this.type = type; // 'INPUT', 'AND', 'OR', 'XOR', 'NOT', 'OUTPUT'
+        this.type = type; 
         this.inputs = [];
         this.value = 0;
     }
-
     evaluate() {
         if (this.type === 'INPUT') return this.value;
         const vals = this.inputs.map(n => n.evaluate());
         if (vals.length === 0) return 0;
-
         switch (this.type) {
             case 'AND': return vals.reduce((a, b) => a & b, 1);
             case 'OR':  return vals.reduce((a, b) => a | b, 0);
@@ -20,12 +19,14 @@ class LogicNode {
             default: return 0;
         }
     }
-
     getExpression() {
-        if (this.type === 'INPUT') return this.id;
+        const formatId = (idStr) => {
+            const parts = idStr.split('_');
+            return `<i>${parts[0]}<sub>${parts[1]}</sub></i>`;
+        };
+        if (this.type === 'INPUT') return formatId(this.id);
         if (this.inputs.length === 0) return '0';
         const exprs = this.inputs.map(n => n.getExpression());
-
         switch (this.type) {
             case 'AND': return `(${exprs.join(' · ')})`;
             case 'OR':  return `(${exprs.join(' + ')})`;
@@ -36,12 +37,10 @@ class LogicNode {
         }
     }
 }
-
 function generateTruthTable(inputNodes, outputNodes) {
     if (inputNodes.length === 0) return [];
     const numRows = Math.pow(2, inputNodes.length);
     const table = [];
-
     for (let i = 0; i < numRows; i++) {
         const row = {};
         inputNodes.forEach((node, idx) => {
